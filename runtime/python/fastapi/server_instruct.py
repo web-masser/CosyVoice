967eba58e27b390cc2f6ef270bc4404c7099605b
mch <13541072472@163.com>
2025-03-01 19:24:54 +0800

    feat

--- runtime/python/fastapi/server_instruct.py
+++ runtime/python/fastapi/server_instruct.py
@@ -295,7 +295,7 @@ if __name__ == '__main__':
         uvicorn.run(
             "server_instruct:app",
             host="0.0.0.0",
-            port=6775,
+            port=6715,
             ssl_keyfile="./mznpy.com.key",
             ssl_certfile="./mznpy.com.pem",
             ws="websockets",
